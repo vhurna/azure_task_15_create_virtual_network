90be83f3989cf904482ce8881a5c201b1d6ad204
mira <mkyshchak@gmail.com>
2025-04-05 22:20:27 +0300

    az15

--- task.ps1
+++ task.ps1
@@ -4,14 +4,25 @@ $resourceGroupName = "mate-azure-task-15"
 $virtualNetworkName = "todoapp"
 $vnetAddressPrefix = "10.20.30.0/24"
 $webSubnetName = "webservers"
-$webSubnetIpRange = "???" # <- calculate subnet ip address range 
+$webSubnetIpRange = "10.20.30.0/26" # Діапазон для підмережі webservers
 $dbSubnetName = "database"
-$dbSubnetIpRange = "???" # <- calculate subnet ip address range 
+$dbSubnetIpRange = "10.20.30.64/26" # Діапазон для підмережі database
 $mngSubnetName = "management"
-$mngSubnetIpRange = "???" # <- calculate subnet ip address range 
+$mngSubnetIpRange = "10.20.30.128/26" # Діапазон для підмережі management
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-Write-Host "Creating a virtual network ..."
-# write your code here -> 
+Write-Host "Creating subnets..."
+$webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange
+$dbSubnet = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange
+$mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange
+
+Write-Host "Creating a virtual network with subnets..."
+$vnet = New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $webSubnet, $dbSubnet, $mngSubnet
+
+Write-Host "Running artifacts generation script..."
+.\scripts\generate-artifacts.ps1
+
+Write-Host "Running validation script..."
+.\scripts\validate-artifacts.ps1
